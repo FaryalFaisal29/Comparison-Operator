54d8ecf856cb8b8f5e2468da113d268cf60ce3ff
FaryalFaisal29 <164797311+FaryalFaisal29@users.noreply.github.com>
2024-03-26 01:14:43 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,18 @@
+// 5 == 2 is false
+// 5 != 2 is true
+// 5 > 2 is true
+// 5 < 2 is false
+
+let a = 5
+let b = 2
+let isEqual = (a == b);
+console.log(isEqual);
+
+let isNotEqual = (a != b);
+console.log(isNotEqual);
+
+let isGreaterThan = (a > b);
+console.log(isGreaterThan);
+
+let isLessThan = (a < b);
+console.log(isLessThan);
